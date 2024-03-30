60313b4043ef4a7f8c54b51851dbb1ebc2421f46
PlebRick <B1ackSwan@Protonmail.com>
2024-03-30 13:33:54 -0500

    update store.ts

--- startos/store.ts
+++ startos/store.ts
@@ -1,9 +1,18 @@
+import { setupExposeStore } from '@start9labs/start-sdk'
+
 /**
  * The Store is for persisting data that are needed by the package but are NOT persisted by the upstream service. Do NOT persist data here that are already being persisted by the service itself.
  *
  * Store data should be kept to a minimum. Stateless packages are easier to maintain and eliminate unexpected behavior.
  */
+
 export type Store = {
+  secretPhrase: string
+  nestedObject: {
     nameLastUpdatedAt: string
-    secretPhrase: string
   }
+}
+
+export const exposedStore = setupExposeStore<Store>((pathBuilder) => [
+  pathBuilder.nestedObject.nameLastUpdatedAt,
+])
